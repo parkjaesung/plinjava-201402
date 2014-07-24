cfecba9a199a26c6ceff173d7d2beb9040d52961
javajigi <javajigi@gmail.com>
2014-07-24 15:04:17 +0900

    사다리 타기 기본 구현 및 예외 처리 완료함.

---
+++ ladder_javajigi/src/ladder/Ladder.java
@@ -0,0 +1,38 @@
+package ladder;
+
+class Ladder {
+	private Row[] rows;
+
+	Ladder(int height, int noOfPerson) {
+		if (height < 1) {
+			throw new IllegalArgumentException(String.format("사다리 높이는 1 이상이어야 합니다. 현재 값은 : %d", height));
+		}
+		
+		rows = new Row[height];
+		
+		for (int i = 0; i < height; i++) {
+			rows[i] = new Row(noOfPerson);
+		}
+	}
+
+	void drawLine(int height, int startPosition) {
+		if (height < 0) {
+			throw new IllegalArgumentException(String.format("사다리 높이는 0이상이어야 합니다. 현재 값 : %d", height));
+		}
+		
+		if (height > rows.length -1) {
+			throw new IllegalArgumentException(String.format("사다리 최대 높이를 넘어섰습니다. 현재 값 : %d", height));
+		}
+		
+		rows[height].drawLine(startPosition);
+	}
+
+	int run(int nthOfPerson) {
+		for (int i = 0; i < rows.length; i++) {
+			Row row = rows[i];
+			nthOfPerson = row.move(nthOfPerson);
+		}
+
+		return nthOfPerson;
+	}
+}
